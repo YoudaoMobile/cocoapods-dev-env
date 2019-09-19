2d09147a69874dac075e14d773867d5d7daa9ef5
吴锡苗 <wuximiao@rd.netease.com>
2019-09-20 00:00:58 +0800

    初始化项目文件

---
+++ lib/cocoapods/dev/env.rb
@@ -0,0 +1,10 @@
+require "cocoapods/dev/env/version"
+
+module Cocoapods
+  module Dev
+    module Env
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cocoapods/dev/env/version.rb
@@ -0,0 +1,7 @@
+module Cocoapods
+  module Dev
+    module Env
+      VERSION = "0.1.0"
+    end
+  end
+end
